a5ec6fdc8394d816bd6500f75944739bcea16e17
kazu638 <k89ut3903@gmail.com>
2020-06-11 14:46:54 +0900

    implementation edit & update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,8 +10,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      # index実装後にtasks_pathに変更
-      redirect_to new_task_path, notice: "タスクの投稿に成功しました。"
+      redirect_to tasks_path, notice: "タスクの投稿に成功しました。"
     else
       flash.now[:alert] = "タスクの作成に失敗しました。"
       render :new
@@ -20,6 +19,20 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクの更新に成功しました。"
+    else
+      flash.now[:alert] = "タスクの更新に失敗しました。"
+      render :edit
+    end
   end
 
   private
