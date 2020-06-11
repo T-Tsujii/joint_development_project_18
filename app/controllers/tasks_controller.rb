064b0afaafba2fa0662fd190d7ab690022093e09
kazu638 <k89ut3903@gmail.com>
2020-06-11 12:53:44 +0900

    implementation new & create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,21 @@
 class TasksController < ApplicationController
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      # index実装後にtasks_pathに変更
+      redirect_to new_task_path, notice: "タスクの投稿に成功しました。"
+    else
+      flash.now[:alert] = "タスクの作成に失敗しました。"
+      render :new
+    end
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:title, :content)
+    end
 end
